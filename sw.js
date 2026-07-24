8db1a8954e699e05c85a4da0c82d78c9f0601713
LoM87 <lore.martinelli@gmail.com>
2026-07-24 14:09:11 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,56 @@
+/* Service worker: tiene l'app in memoria sul telefono così funziona senza rete.
+   Quando aggiorni i file, cambia il numero di versione qui sotto (es. v2, v3...). */
+var CACHE = "rendi-v1";
+var FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-180.png",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./icon-512-maskable.png"
+];
+
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function (c) {
+      return Promise.all(FILES.map(function (f) {
+        return c.add(f).catch(function () { /* un file mancante non blocca l'installazione */ });
+      }));
+    }).then(function () { return self.skipWaiting(); })
+  );
+});
+
+self.addEventListener("activate", function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.map(function (k) {
+        return k === CACHE ? null : caches.delete(k);
+      }));
+    }).then(function () { return self.clients.claim(); })
+  );
+});
+
+self.addEventListener("fetch", function (e) {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(function (hit) {
+      if (hit) {
+        // serve subito dalla cache, poi aggiorna in sottofondo se c'è rete
+        fetch(e.request).then(function (res) {
+          if (res && res.status === 200) caches.open(CACHE).then(function (c) { c.put(e.request, res.clone()); });
+        }).catch(function () {});
+        return hit;
+      }
+      return fetch(e.request).then(function (res) {
+        if (res && res.status === 200 && e.request.url.indexOf("http") === 0) {
+          var copy = res.clone();
+          caches.open(CACHE).then(function (c) { c.put(e.request, copy); });
+        }
+        return res;
+      }).catch(function () {
+        return caches.match("./index.html");
+      });
+    })
+  );
+});
